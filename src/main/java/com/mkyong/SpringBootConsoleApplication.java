f485fbef42c051f5aa7386b93a7dd6e69d74cd50
namuan <575441+namuan@users.noreply.github.com>
2017-11-07 17:20:46 +0000

    [added] spring boot command line application to list AWS queues

---
+++ src/main/java/com/mkyong/SpringBootConsoleApplication.java
@@ -0,0 +1,44 @@
+package com.mkyong;
+
+import static java.lang.System.exit;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.ListQueuesResult;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication implements CommandLineRunner {
+
+    @Bean
+    public AmazonSQS sqsClient() {
+        return AmazonSQSClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+
+    public static void main(String[] args) {
+
+        SpringApplication app = new SpringApplication(SpringBootConsoleApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        final ListQueuesResult listQueuesResult = sqsClient().listQueues();
+        final List<String> queueUrls = listQueuesResult.getQueueUrls();
+        for (String queueUrl : queueUrls) {
+            System.out.println("QueueUrl:" + queueUrl);
+        }
+
+        exit(0);
+    }
+}
